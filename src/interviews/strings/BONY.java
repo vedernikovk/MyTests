a23b6f588d0084a88be62ebda304d5a53ad87624
Konstantin  Vedernikov <vedernik@gmail.com>
2022-07-31 12:27:39 -0400

    String tests added

--- src/interviews/strings/BONY.java
+++ src/interviews/strings/BONY.java
@@ -3,7 +3,8 @@ package interviews.strings;
 import java.util.*;
 
 public class BONY {
-		
+
+	// Substrings of string s of specified size
 	private static Set<String> getSubstrings(String s, int size) {		 
 		Set<String> result = new HashSet<String>();
 		for (int i = 0; i <= s.length() - size; i++) {
@@ -11,7 +12,8 @@ public class BONY {
 		}
 		return result; 
 	}
-	
+
+	// aabbbccccd -> a2b3c4d
 	private static String transforms(String s) {
 		StringBuilder sb = new StringBuilder();
 		
@@ -36,6 +38,7 @@ public class BONY {
 		return sb.toString();
 	}
 
+	// Number of changes to eliminate repetitive characters
 	private static int getSubsCount(String s) {
 		int subsCouner = 0;
 		char prev = s.charAt(0);
@@ -51,64 +54,11 @@ public class BONY {
 		return subsCouner;
 	}
 
-	public static String getSmallestAndLargest(String s, int k) {
-		String smallest = "";
-		String largest = "";
-
-		List<String> subs = new ArrayList<>();
-		for (int i=0; i <= s.length() - k; i++) {
-			String sub = s.substring(i, i + k);
-			subs.add(sub);
-		}
-
-		Collections.sort(subs);
-		smallest = subs.get(0);
-		largest = subs.get(subs.size() - 1);
-
-		return smallest + "\n" + largest;
-	}
-
-	static boolean isAnagram(String a, String b) {
-		if (a.length() != b.length()) {
-			return false;
-		}
-
-		var afrequency = frequency(a.toLowerCase());
-		var bfrequency = frequency(b.toLowerCase());
-
-		for (Map.Entry<Character, Integer> entry : afrequency.entrySet()) {
-			if (!bfrequency.containsKey(entry.getKey())) {
-				return false;
-			}
-
-			if (bfrequency.get(entry.getKey()) != entry.getValue()) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
-	private static Map<Character, Integer> frequency(String s) {
-		Map<Character, Integer> result = new HashMap<>();
-		for (int i = 0; i <= s.length() - 1; i++) {
-			Character c = s.charAt(i);
-			if (result.containsKey(c)) {
-				result.put(c, result.get(c)+1);
-			} else {
-				result.put(c, 1);
-			}
-		}
-
-		return result;
-	}
-
 	public static void main(String[] args) {
-		//System.out.print(getSubstrings("sdkjfsdk", 3));
-		//System.out.println(getSubsCount("aabbbcccdfgqqqz"));
-		//System.out.println(getSubsCount("aabbbcccdfgqqqzz"));
-		//System.out.println(getSubsCount("abbbcccdfgqqqzz"));
-		//System.out.println(getSubsCount("aabbbcccdfgqqqqqqz"));
-		// System.out.println(getSmallestAndLargest("welcometojava" , 3));
+		System.out.println(getSubstrings("sdkjfsdk", 3));
+		System.out.println(getSubsCount("aabbbcccdfgqqqz"));
+		System.out.println(getSubsCount("aabbbcccdfgqqqzz"));
+		System.out.println(getSubsCount("abbbcccdfgqqqzz"));
+		System.out.println(getSubsCount("aabbbcccdfgqqqqqqz"));
 	}
 }
---
+++ src/interviews/strings/Hackerrank.java
@@ -0,0 +1,72 @@
+package interviews.strings;
+
+import java.util.*;
+
+public class Hackerrank {
+
+    /* Given a string s, and an integer, k, complete the function so that it finds
+       the lexicographically smallest and largest substrings of length
+     */
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        List<String> subs = new ArrayList<>();
+        for (int i=0; i <= s.length() - k; i++) {
+            String sub = s.substring(i, i + k);
+            subs.add(sub);
+        }
+
+        Collections.sort(subs);
+        smallest = subs.get(0);
+        largest = subs.get(subs.size() - 1);
+
+        return smallest + "\n" + largest;
+    }
+
+    /* Two strings,  a and , b are called anagrams if they contain all the same characters in the same
+       frequencies. For this challenge, the test is not case-sensitive.
+       For example, the anagrams of CAT are CAT, ACT, tac, TCA, aTC, and CtA.
+     */
+    static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        var afrequency = frequency(a.toLowerCase());
+        var bfrequency = frequency(b.toLowerCase());
+
+        for (Map.Entry<Character, Integer> entry : afrequency.entrySet()) {
+            if (!bfrequency.containsKey(entry.getKey())) {
+                return false;
+            }
+
+            if (bfrequency.get(entry.getKey()) != entry.getValue()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static Map<Character, Integer> frequency(String s) {
+        Map<Character, Integer> result = new HashMap<>();
+        for (int i = 0; i <= s.length() - 1; i++) {
+            Character c = s.charAt(i);
+            if (result.containsKey(c)) {
+                result.put(c, result.get(c)+1);
+            } else {
+                result.put(c, 1);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSmallestAndLargest("welcometojava" , 3));
+
+        boolean ret = isAnagram("anagramm", "marganaa");
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
